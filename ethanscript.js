21ae6017e1e15bdb9c5149f03b6c83f7ad04d636
Ethan1440 <114273406+Ethan1440@users.noreply.github.com>
2024-06-21 10:48:00 -0500

    Update ethanscript.js
    edited resize page event listener

--- ethanscript.js
+++ ethanscript.js
@@ -2123,4 +2123,4 @@ function calcBannerWidth(banner, numDays){
 }
 
 //page listener for window Resize
-document.addEventListener('resize', calcBannerWidthResize())
+document.addEventListener('resize', calcBannerWidthResize, false)
